935d53160f690af15f33b9af22683e8c72447841
Mahima Agrawal <mahimaagrawal@Mahimas-MacBook-Pro.local>
2024-09-23 03:47:38 +0530

    formatting

---
+++ src/Assets/AngleRight.js
@@ -0,0 +1,1 @@
+export const AngleRight =  <svg viewBox="0 0 24 24" focusable="false" height="18" width="18"><path d="M0 0h24v24H0z" fill="none"></path><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></svg>
---
+++ src/Assets/SaveIcon.js
@@ -0,0 +1,2 @@
+export const SaveIcon = <svg viewBox="0 0 24 24" focusable="false" height="18" width="18"><path d="M0 0h24v24H0z" fill="none"></path><path d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z"></path></svg>
+            
